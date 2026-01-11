944314e07959100656c80bd3a8276e3eaffc59db
liudewensss <1442154146@qq.com>
2026-01-11 15:08:25 +0800

    Set default rendezvous server and key

--- src/config.rs
+++ src/config.rs
@@ -106,8 +106,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["114.66.52.77"];
+pub const RS_PUB_KEY: &str = "WX6p8pc7vMTj9xlTbhTfeQPnC8vxfTMD+WRhaaN597M=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
