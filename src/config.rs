8d2652107b8d9883fd8e9c877e6a34bfc69d186d
liudewensss <1442154146@qq.com>
2026-01-11 17:41:07 +0800

    Default enable quality monitor

--- src/config.rs
+++ src/config.rs
@@ -1984,6 +1984,7 @@ impl UserDefaultConfig {
             keys::OPTION_CUSTOM_IMAGE_QUALITY => self.get_num_string(key, 50.0, 10.0, 0xFFF as f64),
             keys::OPTION_CUSTOM_FPS => self.get_num_string(key, 30.0, 5.0, 120.0),
             keys::OPTION_ENABLE_FILE_COPY_PASTE => self.get_string(key, "Y", vec!["", "N"]),
+            keys::OPTION_SHOW_QUALITY_MONITOR => self.get_string(key, "Y", vec!["N"]),
             keys::OPTION_EDGE_SCROLL_EDGE_THICKNESS => self.get_num_string(key, 100, 20, 150),
             keys::OPTION_TRACKPAD_SPEED => self.get_num_string(key, 100, 10, 1000),
             _ => self
